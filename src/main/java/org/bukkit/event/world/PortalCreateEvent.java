9be5c6b0f60d1bab58129f465646d1f30a8f4060
Celtic Minstrel <celtic.minstrel.ca@some.place>
2011-07-14 11:35:12 -0400

    Update for Minecraft 1.6

--- src/main/java/org/bukkit/event/world/PortalCreateEvent.java
+++ src/main/java/org/bukkit/event/world/PortalCreateEvent.java
@@ -11,7 +11,7 @@ import java.util.Collection;
  */
 public class PortalCreateEvent extends WorldEvent implements Cancellable {
     private boolean cancel = false;
-    private ArrayList<Block> blocks = new ArrayList<Block>();
+    private Collection<Block> blocks = new ArrayList<Block>();
 
     public PortalCreateEvent(final Collection<Block> blocks, final World world) {
         super(Type.PORTAL_CREATE, world);
@@ -23,7 +23,7 @@ public class PortalCreateEvent extends WorldEvent implements Cancellable {
      *
      * @return array list of all the blocks associated with the created portal
      */
-    public ArrayList<Block> getBlocks() {
+    public Collection<Block> getBlocks() {
         return this.blocks;
     }
 
